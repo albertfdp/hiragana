b4b5ed59bfd7c3678284dec3bb4cab6563bbc76b
Albert Fernández <albert@nandez.cat>
2019-10-17 20:41:42 +0200

    initial commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { HotModuleReplacementPlugin } = require('webpack');
+
+module.exports = {
+  mode: 'development',
+
+  devtool: 'cheap-eval-source-map',
+
+  entry: {
+    app: ['./src/index.js']
+  },
+
+  output: {
+    filename: '[name].js',
+    publicPath: '/'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    hot: true
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Hiragana'
+    }),
+    new HotModuleReplacementPlugin()
+  ]
+};
